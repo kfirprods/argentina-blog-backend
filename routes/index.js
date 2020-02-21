f551fb72f62191c98dd9c4162604db15447f6dba
Kfir <kfirprods@gmail.com>
2020-02-21 05:35:14 +0200

    Better implementation for destinations and destinations/:id/posts

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,45 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
+const readDir = util.promisify(fs.readdir);
 
 let parameters = new Array();
+
+async function getPostsForDestination(destinationName) {
+  const destinationPostNames = await readDir(`data/destinations/${destinationName}/posts`);
+
+  return destinationPostNames.map(async postName => {
+    const rawPostJson = await readFile(`data/destinations/${destinationName}/posts/${postName}/data.json`);
+    const post = JSON.parse(rawPostJson);
+    post.id = postName;
+    return post;
+  });
+}
+
+router.get('/argentina/destinations', async (req, res, next) => {
+  const destinationNames = await readDir('data/destinations');
+  const destinations = new Array();
+
+  for (let destinationName of destinationNames) {
+    const rawDestinationJson = await readFile(`data/destinations/${destinationName}/data.json`);
+    const destination = JSON.parse(rawDestinationJson);
+    const posts = await Promise.all(await getPostsForDestination(destinationName));
+    destination.id = destinationName;
+    destination.postsCount = posts.length;
+    destination.photosCount = posts.map(post => post.gallery.length).reduce((a,b) => a+b, 0);
+    destinations.push(destination);
+  }
+
+  res.json(destinations);
+});
+
+router.get('/argentina/destinations/:destinationId/posts', async (req, res, next) => {
+  const posts = await Promise.all(await getPostsForDestination(req.params.destinationId));
+  res.json(posts);
+});
 
 for (let depth of new Array("dir1", "dir2", "dir3", "dir4", "dir5")) {
   parameters.push(depth);
@@ -77,7 +114,7 @@ function handleGenericPutRequest(req, res, next) {
     res.send();
     return;
   }
-  
+
   const desiredPath = getPathFromGenericParams(req.params);
   let desiredDataFolder = `data${desiredPath}`;
   let desiredDataFile = `${desiredDataFolder}/data.json`;
@@ -89,7 +126,7 @@ function handleGenericPutRequest(req, res, next) {
     return;
   }
 
-  fs.mkdir(desiredDataFolder, ()=>{
+  fs.mkdir(desiredDataFolder, () => {
     fs.writeFile(desiredDataFile, JSON.stringify(request.data), {}, () => {
       res.status(200);
       res.send();
@@ -116,7 +153,7 @@ function handleGenericDeleteRequest(req, res, next) {
     return;
   }
 
-  fs.rmdir(desiredDataFolder, {recursive: true}, () => {
+  fs.rmdir(desiredDataFolder, { recursive: true }, () => {
     res.status(200);
     res.send();
   });
@@ -141,7 +178,7 @@ function handleGenericPostRequest(req, res, next) {
     return;
   }
 
-  fs.writeFile(desiredDataFile, JSON.stringify(request.data), ()=>{
+  fs.writeFile(desiredDataFile, JSON.stringify(request.data), () => {
     res.status(200);
     res.send();
   });
